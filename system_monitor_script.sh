4485e2ceca8bdef6f9c8412098a7c905ac9ff621
shaif ali khan <shaifalikhan5@gmail.com>
2024-05-18 12:09:33 +0530

    added system monitoring script

---
+++ system_monitor_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+threshold=90
+recipient="shaifalikhan5@gmail.com.com"
+subject="High CPU Usage Detected"
+
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
+
+if [ "$cpu_usage" -lt "$threshold" ]; then
+    echo "High CPU usage detected: $cpu_usage%"
+
+    # Send an email notification
+    echo "High CPU usage detected: $cpu_usage%" | mail -s "$subject" "$recipient"
+fi
